e7747d910e2a29ceb281f05ea9454389cab7665d
M. S. Farzan <doryoku@gmail.com>
2020-08-01 18:38:46 -0700

    Restructured app and incorporated Struct.

---
+++ client/src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+    <div id="home">
+        <h1>Entromancy Companion App</h1>
+        <div class="div-button" v-on:click="displayToggle = 'lore'" v-bind:class="{selected: displayToggle==='lore'}">
+            <p>Lore</p>
+        </div>
+        <div class="div-button" v-on:click="displayToggle = 'characters'" v-bind:class="{selected: displayToggle==='characters'}">
+            <p>Character Generator</p>
+        </div>
+        <div class="div-button" v-on:click="displayToggle = 'struct'" v-bind:class="{selected: displayToggle==='struct'}">
+            <p>Struct</p>
+        </div>
+        <Lore v-show="displayToggle === 'lore'" />
+        <Characters v-show="displayToggle === 'characters'" />
+        <StructApp v-show="displayToggle === 'struct'" />
+    </div>
+</template>
+
+<script>
+    import Lore from "./Lore.vue";
+    import Characters from "./Characters.vue";
+    import StructApp from "./StructApp.vue";
+    export default {
+        name: 'Home',
+        components: {
+            Lore,
+            Characters,
+            StructApp
+        },
+        data: function () {
+            return {
+                displayToggle: ""
+            }
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .div-button {
+        display: inline-block;
+        background: cyan;
+        color: black;
+        width: 300px;
+        height: 30px;
+        margin: auto;
+        margin-left: 10px;
+        margin-bottom: 10px;
+        padding-bottom: 10px;
+        line-height: 5px;
+        border-radius: 10px;
+    }
+    .div-button:hover{
+        background: hotpink;
+        cursor: pointer;
+    }
+    .selected {
+        background: hotpink;
+    }
+</style>
+
---
+++ client/src/components/StructApp.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="struct-app">
+        <div id="game">
+            <Game />
+        </div>
+        <div id="border" />
+        <div id="input">
+            <Struct />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Struct from './Struct.vue';
+    import Game from './Game.vue';
+
+    export default {
+        name: 'StructApp',
+        components: {
+            Struct,
+            Game
+        }
+    }
+</script>
+
+<style scoped>
+    #struct-app {
+        font-family: 'Trebuchet MS';
+        text-align: left;
+        background-color: black;
+        color: cyan;
+        display: flex;
+    }
+    #game {
+        width: 50vw;
+        height: 100vh;
+    }
+    #input {
+        width: 50vw;
+        height: 100vh;
+    }
+    #border {
+        border-right: 2px solid cyan;
+    }
+    @media (max-width: 1000px) {
+        #app {
+            flex-direction: column;
+        }
+        #game {
+            width: 100vw;
+            height: 50vh;
+        }
+        #input {
+            width: 100vw;
+            height: 50vh;
+        }
+    }
+</style>
